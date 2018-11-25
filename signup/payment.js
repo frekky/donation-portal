aa3a175038e38f0955ce424f7494cc0aa2d38ad0
frekk <frekk@ucc.asn.au>
2018-11-25 10:51:29 +0800

    clear out nodejs, make way for django

--- signup/payment.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-
-/* placeholder for handling payment API 
-   could use Square or PayPal */
-
--- user/routes/tlas.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var TLA = require('../models/tlaSchema');
-
-router.get('/:tla', function (req, res, next) {
-    
-});
-
-model.exports = router;
