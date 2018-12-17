edc97c6860b7a0d3a4b60829f7b397b25fec0bb8
frekk <frekk@ucc.asn.au>
2018-12-17 17:05:32 +0800

    reorder URLs so that things work

--- gms/memberdb/urls.py
+++ gms/memberdb/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'memberdb'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:username>/', views.info, name='info'),
     path('register/', views.register, name='register'),
     path('renew/', views.renew, name='renew'),
     path('renew/<str:username>/', views.renew, name='renew'),
+    path('<str:username>/', views.info, name='info'),
 ]
