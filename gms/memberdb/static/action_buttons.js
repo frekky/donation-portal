0b2b3991e499817c4bfbf310fee773cf3a093368
frekk <frekk@ucc.asn.au>
2018-12-21 14:08:38 +0800

    nasty javascript hack to make default admin action dropdown into buttons

---
+++ gms/memberdb/static/action_buttons.js
@@ -0,0 +1,34 @@
+/* from https://stackoverflow.com/questions/37318762/django-admin-display-actions-as-buttons */
+(function ($) {
+
+    function fix_actions() {
+        var container = $('div.actions');
+
+        if (container.find('option').length < 10) {
+            container.find('label, button').hide();
+
+            var buttons = $('<div></div>')
+                .prependTo(container)
+                .css('display', 'inline')
+                .addClass('action-buttons');
+
+            container.find('option:gt(0)').each(function () {
+                $('<button>')
+                    .appendTo(buttons)
+                    .attr('name', this.value)
+                    .addClass('button')
+                    .text(this.text)
+                    .click(function () {
+                        container.find('select')
+                            .find(':selected').attr('selected', '').end()
+                            .find('[value=' + this.name + ']').attr('selected', 'selected');
+                        $('#changelist-form button[name="index"]').click();
+                    });
+            });
+        }
+    };
+
+    $(function () {
+        fix_actions();
+    });
+})(django.jQuery);
