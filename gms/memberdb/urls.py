7fd07f8f3e75b707f836e22568920de16368f83d
frekk <frekk@ucc.asn.au>
2018-12-27 09:30:42 +0800

    added member login page

--- gms/memberdb/urls.py
+++ gms/memberdb/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
-from .register import RegisterView
+from .register import RegisterView, RenewView
 
 app_name = 'memberdb'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('renew/', views.renew, name='renew'),
-    path('renew/<str:username>/', views.renew, name='renew'),
-    path('<str:username>/', views.info, name='info'),
+    path('renew/', RenewView.as_view(), name='renew'),
+    path('home/', views.index, name='home'),
+    path('active/', views.getactive, name='actives'),
+    #path('<str:username>/', views.info, name='info'),
 ]
