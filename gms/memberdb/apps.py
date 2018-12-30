25f33b8a2fb81de46d3f1c9a9c957dabea86c79f
frekk <frekk@ucc.asn.au>
2018-12-30 23:52:43 +0800

    fix app verbose_name(s)

--- gms/memberdb/apps.py
+++ gms/memberdb/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.admin.apps import AdminConfig
 
 class MemberConfig(AppConfig):
     name = 'memberdb'
     label = name
     verbose_name = "UCC Member Database"
-
-class MemberDbAdminConfig(AdminConfig):
-    default_site = 'memberdb.admin.MemberDbAdmin'
